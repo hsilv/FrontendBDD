3767c965faf5f187268d7e0b1f1bc10298ff649b
hsilv <hm.sebastiansilva@gmail.com>
2023-04-14 18:11:13 -0600

    Agregación de mensaje de bienvenida, jwt de User

--- home/home.js
+++ home/home.js
@@ -4,15 +4,19 @@ const ip = "http://50.16.178.129:3000/";
 const logOut = document.getElementById('submitButton');
 var code = parseInt((await checkLogin()));
 
+
 if(code != 0){
     window.location.href = '../login/login.html';
 }else{
     console.log("Todo ok, seguís logeado");
+    console.log(localStorage.getItem("medUser"));
 }
+
 
 logOut.addEventListener('click', () => {
     console.log('clickeado');
     localStorage.setItem("jwt", undefined);
+    console.log(localStorage.getItem("medUser"));
     window.location.href = "../login/login.html";
 })
 
