a8c271dc00346d117dcb2c9d16041bbe28b15634
hsilv <hm.sebastiansilva@gmail.com>
2023-04-15 21:49:09 -0600

    Agregación de datos de doctor

--- home/home.js
+++ home/home.js
@@ -1,36 +1,78 @@
 import "../node_modules/bootstrap/dist/js/bootstrap.js";
 const ip = "http://50.16.178.129:3000/";
 
-const logOut = document.getElementById('submitButton');
-var code = parseInt((await checkLogin()));
+const logOut = document.getElementById("submitButton");
+var code = parseInt(await checkLogin());
+const welcome = document.getElementById("welcomeUser");
+const doctorName = document.getElementById("doctorName");
+const specialty = document.getElementById("specialty");
+const address = document.getElementById("address");
+const colegiate = document.getElementById("colegiate");
+const phone = document.getElementById("phone");
 
+var user = localStorage.getItem("medUser");
 
-if(code != 0){
-    window.location.href = '../login/login.html';
-}else{
-    console.log("Todo ok, seguís logeado");
-    console.log(localStorage.getItem("medUser"));
+if (code != 0) {
+  window.location.href = "../login/login.html";
+} else {
+  initUI();
 }
 
-
-logOut.addEventListener('click', () => {
-    console.log('clickeado');
-    localStorage.setItem("jwt", undefined);
-    console.log(localStorage.getItem("medUser"));
-    window.location.href = "../login/login.html";
-})
+logOut.addEventListener("click", () => {
+  localStorage.setItem("jwt", undefined);
+  window.location.href = "../login/login.html";
+});
 
 async function checkLogin() {
-    return fetch(ip + "checkLog", {
-      method: "POST",
-      headers: {
-        Authorization: localStorage.getItem("jwt"),
-      },
+  return fetch(ip + "checkLog", {
+    method: "POST",
+    headers: {
+      Authorization: localStorage.getItem("jwt"),
+    },
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      return data;
     })
-      .then((response) => response.text())
-      .then((data) => {
-        console.log(data);
-        return data;
-      })
-      .catch((err) => console.log(err));
-  }
+    .catch((err) => console.log(err));
+}
+
+async function initUI() {
+  const data = {
+    username: user,
+  };
+  var id_specialty = {
+    id_specialty: "",
+  };
+  await fetch("http://127.0.0.1:3000/getName", {
+    method: "POST",
+    headers: {
+      Authorization: localStorage.getItem("jwt"),
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      welcome.innerHTML = `Bienvenido, ${data[0].doctor_name}!`;
+      doctorName.innerHTML = data[0].doctor_name;
+      id_specialty.id_specialty =  data[0].id_specialty;
+      address.innerHTML = data[0].direction;
+      phone.innerHTML = '+502 '+data[0].phone_number;
+      colegiate.innerHTML = data[0].collegiate_number;
+    })
+    .catch((err) => console.log(err));
+
+  await fetch("http://127.0.0.1:3000/getSpecialty", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    body: JSON.stringify(id_specialty),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      specialty.innerHTML = data[0].specialty_name;
+    })
+    .catch((err) => console.log(err));
+}
